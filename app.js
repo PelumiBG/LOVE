4a95733e118857f2d427c4131b4f42d9cc420221
PelumiBG <bellojoshua2019@gmail.com>
2026-02-05 15:11:19 -0600

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import cors from 'cors';
+import helmet from 'helmet';
 import messageRoute from './route/message.route.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -9,6 +11,8 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(express.json());
+app.cors({origin: 'http://localhost:7000'});
+app.use(helmet());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'view'));
 
