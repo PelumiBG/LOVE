454835f815b45c9dc3cf6bace0a659beb74912dd
PelumiBG <bellojoshua2019@gmail.com>
2026-02-10 17:36:38 +0100

    changed routes

--- route/message.route.js
+++ route/message.route.js
@@ -3,19 +3,20 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('ques', { name: 'OYENIKE', message: 'JB' });
+    res.render('ques', { name: 'OYENIKE', message: '' });
 });
 
 router.post('/submit', (req, res) => {
-    const { message } = req.body || {};
+    const { message, name } = req.body || {};
+    const userName = name || 'OYENIKE';
     if(!message) {
-        return res.render('ques', { message: 'Error: No message provided' });
+        return res.render('ques', { name: userName, message: 'Error: No answer provided' });
     }
-    if(message || message.toLowerCase() === 'YES') {
-        return res.render('index', { message:'JB' });
-    }else {
-        return res.render('ques', { message });
-    };
+    if(message.toLowerCase() === 'yes') {
+        return res.render('index', { message: userName });
+    } else {
+        return res.render('ques', { name: userName, message: 'Sorry can\'t view this Letter' });
+    }
 });
 
 export default router;
